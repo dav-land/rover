d8311fa727a5a2ebaf66b15c34377f13dd6ba7ed
Davis Landry <pi@raspberrypi>
2019-11-27 19:48:11 -0700

    working go straight without encoder

--- driveForward.js
+++ driveForward.js
@@ -1,18 +1,31 @@
 const Gpio = require('pigpio').Gpio;
 
-const wheelLeft = new Gpio(1, {mode: Gpio.OUTPUT});
-const sleepLeft = new Gpio(12, {mode: Gpio.OUTPUT});
-const dirLeft   = new Gpio(13, {mode: Gpio.OUTPUT});
+const wheelLeft  = new Gpio(18, {mode: Gpio.OUTPUT});
+const sleepLeft  = new Gpio(10, {mode: Gpio.OUTPUT});
+const dirLeft    = new Gpio(9 , {mode: Gpio.OUTPUT});
+const wheelRight = new Gpio(11, {mode: Gpio.OUTPUT});
+const sleepRight = new Gpio(22, {mode: Gpio.OUTPUT});
+const dirRight   = new Gpio(27, {mode: Gpio.OUTPUT});
+const encRight   = new Gpio(2, {mode: Gpio.INPUT,
+				 pullUpDown: Gpio.PUD_DOWN,
+				 edge: Gpio.EITHER_EDGE});
 
-let dutyCycle = 0;
+let dutyCycle = 80;
+let countRight = 0;
+
+encRight.on('interupt', (level) => {
+    countRight = countRight + 1;
+});
+
+
 
 setInterval(() => {
-    console.log(dutyCycle);
+    console.log(countRight);
     wheelLeft.pwmWrite(dutyCycle);
     sleepLeft.digitalWrite(1);
     dirLeft.digitalWrite(0);
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
+    wheelRight.pwmWrite(dutyCycle);
+    sleepRight.digitalWrite(1);
+    dirRight.digitalWrite(0);
+
 }, 20);
